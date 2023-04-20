559fb783e9939726d393ac7df29a28047418113c
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-04-21 01:35:24 +0700

    add decoration on admin users table

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -48,7 +48,7 @@
                                     Verified
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Admin
+                                    Role
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     Action
@@ -88,11 +88,21 @@
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                                         </svg>
+                                        @else
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                                        </svg>
                                         @endif
                                     </p>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <p>{{ $user->is_admin ? 'Admin' : ''}}</p>
+                                    @if ($user->is_admin)
+                                        <p class="text-indigo-500">Admin</p>
+                                    @else
+                                        <p>User</p>
+                                    @endif
                                 </td>
                                 <td class="px-6 py-4">
                                     <div class="flex space-x-3">
