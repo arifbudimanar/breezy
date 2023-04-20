84b37e339e82ba06185e9b9f917fa11e40f42b90
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-04-20 21:06:43 +0700

    update dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User with
         User::factory()->create([
-            'name' => 'Arif Budiman Arrosyid',
-            'email' => 'arifbudimanarrosyid@gmail.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'is_admin' => true,
+            'is_verified' => true,
         ]);
+        // User with no email verification
         User::factory()->create([
-            'name' => 'Not Verified',
-            'email' => 'notverifiedaccount@gmail.com',
+            'name' => 'User 1',
+            'email' => 'user1@user.com',
             'email_verified_at' => null,
+        ]);
+        // User with no verified account
+        User::factory()->create([
+            'name' => 'User 2',
+            'email' => 'user2@user.com',
+            'is_verified' => false,
         ]);
         User::factory(10)->create();
     }
