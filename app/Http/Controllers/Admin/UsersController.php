77b8c2f8d63073ec368f01fdd8312002c47bc00a
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-04-28 00:24:27 +0700

    refactor code

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): View
     {
         $search = request('search');
@@ -31,59 +29,13 @@ class UsersController extends Controller
         return view('admin.users.index', compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
+    public function destroy(User $user): RedirectResponse
     {
         $user->delete();
         return back()->with('success', 'User ' . $user->name . ' deleted successfully!');
     }
 
-    /**
-     * Make user admin.
-     */
-    public function makeAdmin(User $user)
+    public function makeAdmin(User $user): RedirectResponse
     {
         if ($user->email_verified_at == null) {
             return back()->with('warning', 'Error, ' . $user->name . ' email is not verified!');
@@ -97,10 +49,7 @@ class UsersController extends Controller
         return back()->with('success', $user->name . ' made admin successfully!');
     }
 
-    /**
-     * Remove user admin.
-     */
-    public function removeAdmin(User $user)
+    public function removeAdmin(User $user): RedirectResponse
     {
         $user->timestamps = false;
         $user->is_admin = 1;
@@ -108,10 +57,7 @@ class UsersController extends Controller
         return back()->with('success', $user->name . ' remove admin successfully!');
     }
 
-    /**
-     * Verify user.
-     */
-    public function verify(User $user)
+    public function verify(User $user): RedirectResponse
     {
         if ($user->email_verified_at == null) {
             return back()->with('warning', 'Error, ' . $user->name . ' email is not verified!');
@@ -122,10 +68,7 @@ class UsersController extends Controller
         return back()->with('success', $user->name . ' verified successfully!');
     }
 
-    /**
-     * Unverify user. If user is admin, remove admin and unverify.
-     */
-    public function unverify(User $user)
+    public function unverify(User $user): RedirectResponse
     {
         if ($user->is_admin) {
             $user->timestamps = false;
