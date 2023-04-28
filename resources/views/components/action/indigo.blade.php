703d00d4c46793fa075918bb250bd5126c2ad513
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-04-28 09:25:46 +0700

    add underline on hover

--- resources/views/components/action/indigo.blade.php
+++ resources/views/components/action/indigo.blade.php
@@ -1,5 +1,5 @@
 <button {{ $attributes->merge(
     ['type' => 'submit',
-    'class' => 'text-indigo-600 dark:text-indigo-400 whitespace-nowrap']) }}>
+    'class' => 'text-indigo-600 dark:text-indigo-400 hover:underline whitespace-nowrap']) }}>
     {{ $slot }}
 </button>
--- resources/views/components/action/red.blade.php
+++ resources/views/components/action/red.blade.php
@@ -1,5 +1,5 @@
 <button {{ $attributes->merge(
     ['type' => 'submit',
-    'class' => 'text-red-600 dark:text-red-400 whitespace-nowrap']) }}>
+    'class' => 'text-red-600 dark:text-red-400 hover:underline whitespace-nowrap']) }}>
     {{ $slot }}
 </button>
