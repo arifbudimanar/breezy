eac1f17433adcb579bdf73243f24a40a29a8765b
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-04-28 09:25:14 +0700

    add badge

---
+++ resources/views/components/badge/admin.blade.php
@@ -0,0 +1,4 @@
+<span
+    class="inline-flex items-center bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-indigo-900 dark:text-indigo-300">
+    Admin
+</span>
