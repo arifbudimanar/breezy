93f85c4a70935a2ef28eab354dceddcb9b9eba33
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-07-05 01:35:05 +0700

    fix tw merge

--- resources/views/components/card/app.blade.php
+++ resources/views/components/card/app.blade.php
@@ -2,7 +2,6 @@
     class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg border-t sm:border-none border-gray-100 dark:border-gray-700">
     {{ $slot }}
 </div> --}}
-<div {{ $attributes->twMerge(['class'=>'p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg border-t sm:border-none
-    border-gray-100 dark:border-gray-700']) }}>
+<div {{ $attributes->twMerge(['class'=>'p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg border-t sm:border-none border-gray-100 dark:border-gray-700']) }}>
     {{ $slot }}
 </div>
