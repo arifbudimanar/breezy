25dbda1668680019ad97e7176ee6509b20fbf3d1
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-07-26 20:47:46 +0700

    fix blink guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,12 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    <!-- X-Cloak -->
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
