f1479df56c7694455414019a1bd3c343e3e7cbb4
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-07-28 23:48:01 +0700

    wip

--- resources/views/components/subcard/app.blade.php
+++ resources/views/components/subcard/app.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-gray-50 dark:bg-gray-700 rounded-lg p-4 flex flex-col justify-between">
+<div class="bg-gray-100 dark:bg-gray-700 rounded-lg p-4 flex flex-col justify-between">
     {{ $slot }}
 </div>
