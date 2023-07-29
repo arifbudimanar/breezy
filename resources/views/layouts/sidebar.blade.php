20e0e2810eb1484fd14cada67e2c551d3b1560b7
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-07-29 07:10:30 +0700

    add sidebar decoration app name

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,13 @@
 <div class="hidden sm:flex border-r border-gray-100 dark:border-gray-700">
     <div class="bg-white dark:bg-gray-800">
-        <div class="sm:w-40 lg:w-56">
+        <div class="px-4 pt-4">
+            <div class="flex pl-2 items-center">
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">
+                    {{ config('app.name', 'Laravel') }}
+                </div>
+            </div>
+        </div>
+        <div class="sm:w-40 lg:w-56 space-y-1 mt-3">
             <x-sidebar-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
             </x-sidebar-nav-link>
