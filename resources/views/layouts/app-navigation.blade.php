81379c32f0fcfb3a2aa39fff82945f6782833bf7
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-07-30 21:56:03 +0700

    👌 IMPROVE: icon navigation

--- resources/views/layouts/app-navigation.blade.php
+++ resources/views/layouts/app-navigation.blade.php
@@ -4,14 +4,14 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex sm:hidden md:flex items-center">
                     <a href="{{ route('home') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px md:ml-10 sm:flex">
                     <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
