6a6f5063af302ef3d6848b930e0ec43b65f53636
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-07-30 19:00:10 +0700

    👌 IMPROVE: add intended url for register

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Providers\RouteServiceProvider;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -17,6 +19,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
+        Redirect::setIntendedUrl(URL::previous());
         return view('auth.login');
     }
 
