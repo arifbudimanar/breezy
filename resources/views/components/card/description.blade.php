379773a0cbda7a93de5d2c0d0905a753a0cf54f6
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-08-02 20:36:38 +0700

    👌 IMPROVE: fix title and description card

--- resources/views/components/card/description.blade.php
+++ resources/views/components/card/description.blade.php
@@ -1,3 +1,3 @@
-<p class="text-sm text-gray-600 dark:text-gray-400 max-w-xl">
+<p class="text-base text-gray-600 dark:text-gray-400 w-full">
     {{ $slot }}
 </p>
--- resources/views/components/card/title.blade.php
+++ resources/views/components/card/title.blade.php
@@ -1,3 +1,3 @@
-<h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 py-1">
+<h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 py-1 pb-3">
     {{ $slot }}
 </h2>
