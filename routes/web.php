9daf3e5fe8b6b6f0c976a9111d636b1665fbbde2
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-08-03 06:58:46 +0700

    👌 IMPROVE: rewrite web route

--- routes/web.php
+++ routes/web.php
@@ -18,35 +18,38 @@ use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 |
 */
 
-Route::view('/', 'welcome')->name('welcome')->middleware('guest');
-Route::view('/home', 'home')->name('home');
-
+// Route::view('/', 'welcome')->name('welcome')->middleware('guest');
+Route::view('/', 'home')->name('home');
 Route::get('/lang/{lang}', [LanguageController::class, 'switchLang'])->name('lang.switch');
-
-Route::prefix('user')->name('user.')->middleware(['auth'])->group(function () {
-    Route::middleware(['verified_email'])->group(function () {
-        Route::get('/dashboard', UserDashboardController::class)->name('dashboard');
+Route::middleware('auth')
+    ->name('user.')
+    ->group(function () {
         Route::get('/profile', [UserProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [UserProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [UserProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+Route::prefix('user')
+    ->name('user.')
+    ->middleware(['auth', 'verified_email'])
+    ->group(function () {
+        Route::get('/dashboard', UserDashboardController::class)->name('dashboard');
+        // Routes that require an auth and verified email go here
         Route::middleware('verified_account')->group(function () {
-            // Other routes here
+            // Other routes that require an auth, verified email and verified account go here
         });
     });
-});
-
-Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin', 'verified_email', 'verified_account'])->group(function () {
-    Route::get('/dashboard', AdminDashboardController::class)->name('dashboard');
-    Route::resource('users', AdminUserController::class)
-        ->only(['index', 'create', 'store', 'show', 'edit', 'update', 'destroy'])
-        ->names([
-            'index' => 'users.index', 'create' => 'users.create', 'store' => 'users.store', 'show' => 'users.show', 'edit' => 'users.edit', 'update' => 'users.update', 'destroy' => 'users.destroy',
-        ]);
-    Route::patch('/users/{user}/make-admin', [AdminUserController::class, 'makeAdmin'])->name('users.makeadmin');
-    Route::patch('/users/{user}/remove-admin', [AdminUserController::class, 'removeAdmin'])->name('users.removeadmin');
-    Route::patch('/users/{user}/verify', [AdminUserController::class, 'verify'])->name('users.verify');
-    Route::patch('/users/{user}/unverify', [AdminUserController::class, 'unverify'])->name('users.unverify');
-    Route::patch('/users/{user}/reset-password', [AdminUserController::class, 'resetPassword'])->name('users.resetpassword');
-});
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware(['auth', 'admin'])
+    ->group(function () {
+        Route::get('/dashboard', AdminDashboardController::class)->name('dashboard');
+        Route::resource('users', AdminUserController::class);
+        Route::patch('/users/{user}/make-admin', [AdminUserController::class, 'makeAdmin'])->name('users.makeadmin');
+        Route::patch('/users/{user}/remove-admin', [AdminUserController::class, 'removeAdmin'])->name('users.removeadmin');
+        Route::patch('/users/{user}/verify', [AdminUserController::class, 'verify'])->name('users.verify');
+        Route::patch('/users/{user}/unverify', [AdminUserController::class, 'unverify'])->name('users.unverify');
+        Route::patch('/users/{user}/reset-password', [AdminUserController::class, 'resetPassword'])->name('users.resetpassword');
+        // Routes that require an auth, admin, verified email and verified account go here
+    });
 
 require __DIR__ . '/auth.php';
